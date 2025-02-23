b4a426983af638a359dcdb2f986bc26490de9e74
Gz1st <Dmitro12082004@gmail.com>
2025-02-23 17:53:24 +0200

    aaa

--- index.php
+++ index.php
@@ -199,16 +199,9 @@ a:visited {
     
 </head>
 <body>
-    <header>
-        <img class="img_logo_vital_blooom" src="img_for_index_html/IMG_9732-Photoroom.png" alt="">
-        <div class="search-bar">
-            <input type="text" id="search-input" placeholder="Пошук товару..." onkeyup="search()">
-            <button onclick="search()"><i class='bx bx-search'></i></button>
-            <div id="search-results" class="search-results">
-                <ul id="search-list"></ul>
-            </div>
-        </div>
-    </header>
+   
+
+  <?php >
 
 <br>
 <br>
